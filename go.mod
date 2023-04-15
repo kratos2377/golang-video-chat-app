e62273576a64a6f5685caf3c8a589733f6c9638d
kratos2377 <shobityadav23@gmail.com>
2023-04-16 02:05:34 +0530

    Stream and room.go handlers completed

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gofiber/template v1.8.0
 	github.com/gofiber/websocket/v2 v2.1.5
 	github.com/google/uuid v1.3.0
+	github.com/pion/rtcp v1.2.10
 	github.com/pion/webrtc/v3 v3.1.59
 	golang.org/x/net v0.9.0
 )
@@ -26,7 +27,6 @@ require (
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/mdns v0.0.7 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
-	github.com/pion/rtcp v1.2.10 // indirect
 	github.com/pion/rtp v1.7.13 // indirect
 	github.com/pion/sctp v1.8.6 // indirect
 	github.com/pion/sdp/v3 v3.0.6 // indirect
--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -3,6 +3,9 @@ package handlers
 import (
 	"fmt"
 	"os"
+	"time"
+	"videochat/pkg/chat"
+	"videochat/pkg/webrtc"
 	w "videochat/pkg/webrtc"
 
 	"github.com/gofiber/fiber/v2"
@@ -48,7 +51,31 @@ func RoomWebSocket(c *websocket.Conn) {
 }
 
 func createOrGetRoom(uuid string) (string, string, *w.Room) {
+	w.RoomsLock.Lock()
+	defer w.RoomsLock.Unlock()
+	h := sha256.new()
+	h.Write([]byte(uuid))
+	suuid := fmt.Sprintf("%x", h.Sum(nil))
 
+	if room := w.Rooms[uuid]; room != nil {
+		if _, ok := w.Streams[suuid]; !ok {
+			w.Streams[suuid] = room
+		}
+		return uuid, suuid, room
+	}
+
+	hub := chat.NewHub()
+	p := &w.Peers{}
+	p.TrackLocals = make(map[string]*webrtc.TrackLocalStaticRTP)
+	room := &w.Room{
+		Peers: p,
+		Hub:   hub,
+	}
+
+	w.Rooms[uuid] = room
+	w.Streams[uuid] = room
+	go hub.Run()
+	return uuid, suuid, room
 }
 
 func RoomViewerWebsocket(c *websocket.Conn) {
@@ -69,7 +96,20 @@ func RoomViewerWebsocket(c *websocket.Conn) {
 }
 
 func roomViewerConn(c *websocket.Conn, p *w.Peers) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	defer c.Close()
 
+	for {
+		select {
+		case <-ticker.C:
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connections))))
+		}
+	}
 }
 
 type websocketMessage struct {
